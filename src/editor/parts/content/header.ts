42bb6759cc1caba2b048cd0bb972f44c882628b9
Randy Merrill <randy@blinkk.com>
2021-01-25 23:03:56 -0700

    Enable tabs for the content sections.

--- src/editor/parts/content/header.ts
+++ src/editor/parts/content/header.ts
@@ -1,5 +1,10 @@
 import {BasePart, Part} from '..';
-import {TemplateResult, expandClasses, html} from '@blinkk/selective-edit';
+import {
+  TemplateResult,
+  expandClasses,
+  html,
+  repeat,
+} from '@blinkk/selective-edit';
 import {ContentSectionPart} from './index';
 import {LiveEditor} from '../../editor';
 
@@ -20,9 +25,31 @@ export class ContentHeaderPart extends BasePart implements Part {
     return classes;
   }
 
+  handleSectionClick(evt: Event, section: ContentSectionPart) {
+    for (const sectionPart of this.config.sections) {
+      sectionPart.isVisible = false;
+    }
+    section.isVisible = true;
+    this.render();
+  }
+
   template(editor: LiveEditor): TemplateResult {
     return html`<div class=${expandClasses(this.classesForPart())}>
-      <div class="le__part__content__header__sections">...sections...</div>
+      <div class="le__part__content__header__sections">
+        ${repeat(
+          this.config.sections,
+          section => section.section,
+          section =>
+            html`<div
+              class="le__part__content__header__section le__clickable ${section.isVisible
+                ? 'le__part__content__header__section--selected'
+                : ''}"
+              @click=${(evt: Event) => this.handleSectionClick(evt, section)}
+            >
+              ${section.label}
+            </div>`
+        )}
+      </div>
       <div class="le__part__content__header__actions">
         <button class="le__button le__button--primary">Save changes</button>
       </div>

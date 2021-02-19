df5aa2406b064915ad01ee022d314feeecba6526
Randy Merrill <randy@blinkk.com>
2021-02-18 18:05:17 -0700

    Example 'pages' with an example aside for code samples.
    Generic aside for displaying information apart from the normal help text from markdown.

---
+++ src/editor/field/aside.ts
@@ -0,0 +1,56 @@
+import {
+  DeepObject,
+  Field,
+  FieldConfig,
+  SelectiveEditor,
+  TemplateResult,
+  Types,
+  html,
+  unsafeHTML,
+} from '@blinkk/selective-edit';
+import {LiveEditorGlobalConfig} from '../editor';
+import marked from 'marked';
+
+export interface AsideFieldConfig extends FieldConfig {
+  /**
+   * Source markdown to be converted to html in the editor.
+   */
+  source: string;
+}
+
+export class AsideField extends Field {
+  config: AsideFieldConfig;
+
+  constructor(
+    types: Types,
+    config: AsideFieldConfig,
+    globalConfig: LiveEditorGlobalConfig,
+    fieldType = 'aside'
+  ) {
+    super(types, config, globalConfig, fieldType);
+    this.config = config;
+  }
+
+  /**
+   * Template for determining how to render the field.
+   *
+   * @param editor Selective editor used to render the template.
+   * @param data Data provided to render the template.
+   */
+  template(editor: SelectiveEditor, data: DeepObject): TemplateResult {
+    return this.templateWrapper(editor, data);
+  }
+
+  /**
+   * Template for rendering the field structure.
+   *
+   * Used for controlling the order that parts of the field are rendered.
+   *
+   * @param editor Selective editor used to render the template.
+   * @param data Data provided to render the template.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  templateStructure(editor: SelectiveEditor, data: DeepObject): TemplateResult {
+    return html`${unsafeHTML(marked(this.config.source))}`;
+  }
+}

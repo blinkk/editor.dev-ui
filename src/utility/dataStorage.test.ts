afb6fa0ebaa792dc44ea75d8e8e70e2698e558ba
Randy Merrill <randy@blinkk.com>
2021-03-20 13:54:46 -0600

    Adding tests for Session Storage.

--- src/utility/dataStorage.test.ts
+++ src/utility/dataStorage.test.ts
@@ -1,55 +1,109 @@
-import {DataStorage} from './dataStorage';
+import {DataStorage, SessionDataStorage} from './dataStorage';
 import test from 'ava';
 
-test('clear no-op in tests', t => {
+test('DataStorage clear no-op in tests', t => {
   const storage = new DataStorage(true);
   t.is(storage.clear(), undefined);
 });
 
-test('getItem null in tests', t => {
+test('DataStorage getItem null in tests', t => {
   const storage = new DataStorage(true);
   t.is(storage.getItem('test'), null);
 });
 
-test('getItemArray empty in tests', t => {
+test('DataStorage getItemArray empty in tests', t => {
   const storage = new DataStorage(true);
   t.deepEqual(storage.getItemArray('test'), []);
 });
 
-test('getItemBoolean default in tests', t => {
+test('DataStorage getItemBoolean default in tests', t => {
   const storage = new DataStorage(true);
   t.is(storage.getItemBoolean('test'), false);
   t.is(storage.getItemBoolean('test', true), true);
 });
 
-test('getItemRecord empty in tests', t => {
+test('DataStorage getItemRecord empty in tests', t => {
   const storage = new DataStorage(true);
   t.deepEqual(storage.getItemRecord('test'), {});
 });
 
-test('setItem no-op in tests', t => {
+test('DataStorage setItem no-op in tests', t => {
   const storage = new DataStorage(true);
   t.is(storage.getItem('test'), null);
   storage.setItem('test', 'foo');
   t.is(storage.getItem('test'), null);
 });
 
-test('setItemArray no-op in tests', t => {
+test('DataStorage setItemArray no-op in tests', t => {
   const storage = new DataStorage(true);
   t.deepEqual(storage.getItemArray('test'), []);
   storage.setItemArray('test', ['foo']);
   t.deepEqual(storage.getItemArray('test'), []);
 });
 
-test('setItemBoolean no-op in tests', t => {
+test('DataStorage setItemBoolean no-op in tests', t => {
   const storage = new DataStorage(true);
   t.is(storage.getItemBoolean('test'), false);
   storage.setItemBoolean('test', true);
   t.is(storage.getItemBoolean('test'), false);
 });
 
-test('setItemRecord no-op in tests', t => {
+test('DataStorage setItemRecord no-op in tests', t => {
   const storage = new DataStorage(true);
+  t.deepEqual(storage.getItemRecord('test'), {});
+  storage.setItemRecord('test', {foo: 'bar'});
+  t.deepEqual(storage.getItemRecord('test'), {});
+});
+
+test('SessionStorage clear no-op in tests', t => {
+  const storage = new SessionDataStorage(true);
+  t.is(storage.clear(), undefined);
+});
+
+test('SessionStorage getItem null in tests', t => {
+  const storage = new SessionDataStorage(true);
+  t.is(storage.getItem('test'), null);
+});
+
+test('SessionStorage getItemArray empty in tests', t => {
+  const storage = new SessionDataStorage(true);
+  t.deepEqual(storage.getItemArray('test'), []);
+});
+
+test('SessionStorage getItemBoolean default in tests', t => {
+  const storage = new SessionDataStorage(true);
+  t.is(storage.getItemBoolean('test'), false);
+  t.is(storage.getItemBoolean('test', true), true);
+});
+
+test('SessionStorage getItemRecord empty in tests', t => {
+  const storage = new SessionDataStorage(true);
+  t.deepEqual(storage.getItemRecord('test'), {});
+});
+
+test('SessionStorage setItem no-op in tests', t => {
+  const storage = new SessionDataStorage(true);
+  t.is(storage.getItem('test'), null);
+  storage.setItem('test', 'foo');
+  t.is(storage.getItem('test'), null);
+});
+
+test('SessionStorage setItemArray no-op in tests', t => {
+  const storage = new SessionDataStorage(true);
+  t.deepEqual(storage.getItemArray('test'), []);
+  storage.setItemArray('test', ['foo']);
+  t.deepEqual(storage.getItemArray('test'), []);
+});
+
+test('SessionStorage setItemBoolean no-op in tests', t => {
+  const storage = new SessionDataStorage(true);
+  t.is(storage.getItemBoolean('test'), false);
+  storage.setItemBoolean('test', true);
+  t.is(storage.getItemBoolean('test'), false);
+});
+
+test('SessionStorage setItemRecord no-op in tests', t => {
+  const storage = new SessionDataStorage(true);
   t.deepEqual(storage.getItemRecord('test'), {});
   storage.setItemRecord('test', {foo: 'bar'});
   t.deepEqual(storage.getItemRecord('test'), {});

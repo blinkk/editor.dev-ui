dc701148ddc428da2ce3d72e88062df6d0152222
Randy Merrill <randy@blinkk.com>
2021-03-29 16:50:04 -0600

    Adding document field type to grow project type.

--- src/projectType/grow/growProjectType.ts
+++ src/projectType/grow/growProjectType.ts
@@ -1,5 +1,13 @@
+import {FieldConstructor} from '@blinkk/selective-edit';
+import {GrowDocumentField} from './field/document';
 import {ProjectTypeComponent} from '../projectType';
 
 export class GrowProjectType implements ProjectTypeComponent {
   type = 'grow';
+
+  get fieldTypes(): Record<string, FieldConstructor> {
+    return {
+      document: (GrowDocumentField as unknown) as FieldConstructor,
+    };
+  }
 }

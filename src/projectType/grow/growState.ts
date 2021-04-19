25a2985892791b0b57814f0fb5f2ecd788412f43
Randy Merrill <randy@blinkk.com>
2021-04-19 16:56:18 -0600

    Project type specific apis (#28)
    Ability to add apis specific to a project type.

---
+++ src/projectType/grow/growState.ts
@@ -0,0 +1,34 @@
+import {ApiError, GrowPartialData, catchError} from '../../editor/api';
+import {BaseProjectTypeState} from '../state';
+
+export class GrowState extends BaseProjectTypeState {
+  partials?: Array<GrowPartialData>;
+
+  get api() {
+    return this.editorState.api;
+  }
+
+  getPartials(
+    callback?: (devices: Array<GrowPartialData>) => void,
+    callbackError?: (error: ApiError) => void
+  ): Array<GrowPartialData> | undefined {
+    const promiseKey = 'getPartials';
+    if (this.promises[promiseKey]) {
+      return;
+    }
+    this.promises[promiseKey] = this.api.projectTypes.grow
+      .getPartials()
+      .then(data => {
+        this.partials = data;
+
+        delete this.promises[promiseKey];
+        if (callback) {
+          callback(this.partials || []);
+        }
+        this.triggerListener(promiseKey);
+        this.render();
+      })
+      .catch(error => catchError(error, callbackError));
+    return this.partials;
+  }
+}

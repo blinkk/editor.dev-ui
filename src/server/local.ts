081d5d91d5b5c76367a938689d03c9a76d464177
Randy Merrill <randy@blinkk.com>
2021-04-27 10:02:31 -0600

    Copy to clipboard on click for npx command.

--- src/server/local.ts
+++ src/server/local.ts
@@ -1,5 +1,6 @@
 import {TemplateResult, html, render} from '@blinkk/selective-edit';
 
+const COPIED_CLASS = 'le__local__command__input--copied';
 const DEFAULT_PORT = 9090;
 
 export interface LocalStatusOptions {
@@ -28,14 +29,36 @@ export class LocalStatus {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   template(localStatus: LocalStatus): TemplateResult {
+    let inputTimeout: undefined | number = undefined;
+
+    const handleCommandClick = (evt: Event) => {
+      // Clean any existing input timeout.
+      if (inputTimeout) {
+        window.clearTimeout(inputTimeout);
+      }
+
+      const target = evt.target as HTMLInputElement;
+      if (target) {
+        target.select();
+        document.execCommand('copy');
+
+        const inputContainer = target.parentElement;
+        inputContainer?.classList.add(COPIED_CLASS);
+
+        inputTimeout = window.setTimeout(() => {
+          inputContainer?.classList.remove(COPIED_CLASS);
+        }, 10000);
+      }
+    };
+
     return html`<div class="le">
       <div class="le__local">
         <h2>Local editor not found</h2>
         <p>To use the editor with a local project run the following command in the main directory of your local project:</p>
-        <p><pre><code><span class="le__local__version"># Requires Node.js >= 14</span>
-npx @blinkk/editor.dev ${
-      this.isNonDefaultPort ? `--port ${this.options?.port}` : ''
-    }</code></pre></p>
+        <p><pre><code><span class="le__local__version"># Requires <a href="https://nodejs.org/" target="_blank">Node.js</a> &gt;= 14</span>
+<div class="le__local__command__input"><input class="le__local__command" readonly @click=${handleCommandClick} value="npx @blinkk/editor.dev${
+      this.isNonDefaultPort ? ` --port ${this.options?.port}` : ''
+    }" /></div></code></pre></p>
       </div>
     </div>`;
   }

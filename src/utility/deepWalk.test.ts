2559adf80ac28f17efeabbbd47f562cf61641916
Randy Merrill <randy@blinkk.com>
2021-04-30 11:05:14 -0600

    Simplify the deepWalk arguments in favor of extending DeepWalk. (#38)

--- src/utility/deepWalk.test.ts
+++ src/utility/deepWalk.test.ts
@@ -1,12 +1,4 @@
-import {
-  DeepWalk,
-  TransformArrayFunction,
-  TransformArrayFunctionSync,
-  TransformFunction,
-  TransformFunctionSync,
-  TransformRecordFunction,
-  TransformRecordFunctionSync,
-} from './deepWalk';
+import {DeepWalk, TransformFunction, TransformFunctionSync} from './deepWalk';
 import {DataType} from '@blinkk/selective-edit/dist/src/utility/dataType';
 import test from 'ava';
 
@@ -69,24 +61,24 @@ test('selectively replace strings in array', async t => {
 
 test('selectively replace array custom', async t => {
   t.plan(1);
-  const walker = new DeepWalk({});
+
+  class TestCustomWalker extends DeepWalk {
+    protected async walkArray(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      originalValue: Array<any>,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      transformValue: TransformFunction
+    ) {
+      return ['foo'];
+    }
+  }
+  const walker = new TestCustomWalker({});
+
   const transformValue = async (value: any) => {
     if (DataType.isString(value)) {
       return `foo${value}`;
     }
     return value;
-  };
-  const transformArray = async (
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    originalValue: Array<any>,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformValue: TransformFunction,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformArray: TransformArrayFunction,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformRecord: TransformRecordFunction
-  ) => {
-    return ['foo'];
   };
 
   t.deepEqual(
@@ -99,8 +91,7 @@ test('selectively replace array custom', async t => {
         bar: 1,
         baz: 'boo',
       },
-      transformValue,
-      transformArray
+      transformValue
     ),
     {
       foo: ['foo'],
@@ -115,26 +106,26 @@ test('selectively replace array custom', async t => {
 
 test('selectively replace objects custom', async t => {
   t.plan(1);
-  const walker = new DeepWalk({});
+
+  class TestCustomWalker extends DeepWalk {
+    protected async walkRecord(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      originalValue: Record<string, any>,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      transformValue: TransformFunction
+    ) {
+      return {
+        foo: true,
+      };
+    }
+  }
+  const walker = new TestCustomWalker({});
+
   const transformValue = async (value: any) => {
     if (DataType.isString(value)) {
       return `foo${value}`;
     }
     return value;
-  };
-  const transformRecord = async (
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    originalValue: Record<string, any>,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformValue: TransformFunction,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformArray: TransformArrayFunction,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformRecord: TransformRecordFunction
-  ) => {
-    return {
-      foo: true,
-    };
   };
 
   t.deepEqual(
@@ -147,9 +138,7 @@ test('selectively replace objects custom', async t => {
         bar: 1,
         baz: 'boo',
       },
-      transformValue,
-      undefined,
-      transformRecord
+      transformValue
     ),
     {
       foo: true,
@@ -217,24 +206,24 @@ test('selectively replace strings in array sync', t => {
 
 test('selectively replace array custom sync', t => {
   t.plan(1);
-  const walker = new DeepWalk({});
+
+  class TestCustomWalker extends DeepWalk {
+    protected walkArraySync(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      originalValue: Array<any>,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      transformValue: TransformFunctionSync
+    ) {
+      return ['foo'];
+    }
+  }
+  const walker = new TestCustomWalker({});
+
   const transformValue = (value: any) => {
     if (DataType.isString(value)) {
       return `foo${value}`;
     }
     return value;
-  };
-  const transformArray = (
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    originalValue: Array<any>,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformValue: TransformFunctionSync,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformArray: TransformArrayFunctionSync,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformRecord: TransformRecordFunctionSync
-  ) => {
-    return ['foo'];
   };
 
   t.deepEqual(
@@ -247,8 +236,7 @@ test('selectively replace array custom sync', t => {
         bar: 1,
         baz: 'boo',
       },
-      transformValue,
-      transformArray
+      transformValue
     ),
     {
       foo: ['foo'],
@@ -263,26 +251,26 @@ test('selectively replace array custom sync', t => {
 
 test('selectively replace objects custom sync', t => {
   t.plan(1);
-  const walker = new DeepWalk({});
+
+  class TestCustomWalker extends DeepWalk {
+    protected walkRecordSync(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      originalValue: Record<string, any>,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      transformValue: TransformFunctionSync
+    ) {
+      return {
+        foo: true,
+      };
+    }
+  }
+  const walker = new TestCustomWalker({});
+
   const transformValue = async (value: any) => {
     if (DataType.isString(value)) {
       return `foo${value}`;
     }
     return value;
-  };
-  const transformRecord = (
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    originalValue: Record<string, any>,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformValue: TransformFunctionSync,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformArray: TransformArrayFunctionSync,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    transformRecord: TransformRecordFunctionSync
-  ) => {
-    return {
-      foo: true,
-    };
   };
 
   t.deepEqual(
@@ -295,9 +283,7 @@ test('selectively replace objects custom sync', t => {
         bar: 1,
         baz: 'boo',
       },
-      transformValue,
-      undefined,
-      transformRecord
+      transformValue
     ),
     {
       foo: true,
--- src/utility/deepWalk.ts
+++ src/utility/deepWalk.ts
@@ -6,32 +6,7 @@ export interface DeepWalkConfig {}
 export type WalkableType = Record<string, any> | Array<any>;
 
 export type TransformFunction = (value: any) => Promise<any>;
-export type TransformArrayFunction = (
-  originalValue: Array<any>,
-  transformValue: TransformFunction,
-  transformArray: TransformArrayFunction,
-  transformRecord: TransformRecordFunction
-) => Promise<Array<any>>;
-export type TransformRecordFunction = (
-  originalValue: Record<string, any>,
-  transformValue: TransformFunction,
-  transformArray: TransformArrayFunction,
-  transformRecord: TransformRecordFunction
-) => Promise<Record<string, any>>;
-
 export type TransformFunctionSync = (value: any) => any;
-export type TransformArrayFunctionSync = (
-  originalValue: Array<any>,
-  transformValue: TransformFunctionSync,
-  transformArray: TransformArrayFunctionSync,
-  transformRecord: TransformRecordFunctionSync
-) => Array<any>;
-export type TransformRecordFunctionSync = (
-  originalValue: Record<string, any>,
-  transformValue: TransformFunctionSync,
-  transformArray: TransformArrayFunctionSync,
-  transformRecord: TransformRecordFunctionSync
-) => Record<string, any>;
 
 export class DeepWalk {
   config: DeepWalkConfig;
@@ -42,86 +17,35 @@ export class DeepWalk {
 
   async walk(
     value: WalkableType,
-    transformValue: TransformFunction,
-    transformArray?: TransformArrayFunction,
-    transformRecord?: TransformRecordFunction
+    transformValue: TransformFunction
   ): Promise<WalkableType> {
-    if (!transformArray) {
-      transformArray = this.walkArray;
-    }
-    if (!transformRecord) {
-      transformRecord = this.walkRecord;
-    }
-
     if (DataType.isArray(value)) {
-      return await transformArray(
-        value as Array<any>,
-        transformValue,
-        transformArray,
-        transformRecord
-      );
+      return await this.walkArray(value as Array<any>, transformValue);
     }
-    return await transformRecord(
-      value as Record<string, any>,
-      transformValue,
-      transformArray,
-      transformRecord
-    );
+    return await this.walkRecord(value as Record<string, any>, transformValue);
   }
 
   walkSync(
     value: WalkableType,
-    transformValue: TransformFunctionSync,
-    transformArray?: TransformArrayFunctionSync,
-    transformRecord?: TransformRecordFunctionSync
+    transformValue: TransformFunctionSync
   ): WalkableType {
-    if (!transformArray) {
-      transformArray = this.walkArraySync;
-    }
-    if (!transformRecord) {
-      transformRecord = this.walkRecordSync;
-    }
-
     if (DataType.isArray(value)) {
-      return transformArray(
-        value as Array<any>,
-        transformValue,
-        transformArray,
-        transformRecord
-      );
+      return this.walkArraySync(value as Array<any>, transformValue);
     }
-    return transformRecord(
-      value as Record<string, any>,
-      transformValue,
-      transformArray,
-      transformRecord
-    );
+    return this.walkRecordSync(value as Record<string, any>, transformValue);
   }
 
   protected async walkArray(
     originalValue: Array<any>,
-    transformValue: TransformFunction,
-    transformArray: TransformArrayFunction,
-    transformRecord: TransformRecordFunction
+    transformValue: TransformFunction
   ): Promise<Array<any>> {
     const newValue: Array<any> = [];
-
     for (let value of originalValue) {
       if (DataType.isObject(value)) {
         // Clean in depth before testing for walking.
-        value = await transformRecord(
-          value,
-          transformValue,
-          transformArray,
-          transformRecord
-        );
+        value = await this.walkRecord(value, transformValue);
       } else if (DataType.isArray(value)) {
-        value = await transformArray(
-          value as Array<any>,
-          transformValue,
-          transformArray,
-          transformRecord
-        );
+        value = await this.walkArray(value as Array<any>, transformValue);
       } else {
         value = await transformValue(value);
       }
@@ -133,28 +57,15 @@ export class DeepWalk {
 
   protected walkArraySync(
     originalValue: Array<any>,
-    transformValue: TransformFunctionSync,
-    transformArray: TransformArrayFunctionSync,
-    transformRecord: TransformRecordFunctionSync
+    transformValue: TransformFunctionSync
   ): Array<any> {
     const newValue: Array<any> = [];
-
     for (let value of originalValue) {
       if (DataType.isObject(value)) {
         // Clean in depth before testing for walking.
-        value = transformRecord(
-          value,
-          transformValue,
-          transformArray,
-          transformRecord
-        );
+        value = this.walkRecordSync(value, transformValue);
       } else if (DataType.isArray(value)) {
-        value = transformArray(
-          value as Array<any>,
-          transformValue,
-          transformArray,
-          transformRecord
-        );
+        value = this.walkArraySync(value as Array<any>, transformValue);
       } else {
         value = transformValue(value);
       }
@@ -165,29 +76,16 @@ export class DeepWalk {
 
   protected async walkRecord(
     originalValue: Record<string, any>,
-    transformValue: TransformFunction,
-    transformArray: TransformArrayFunction,
-    transformRecord: TransformRecordFunction
+    transformValue: TransformFunction
   ): Promise<Record<string, any>> {
     const newValue: Record<string, any> = {};
-
     // eslint-disable-next-line prefer-const
     for (let [key, value] of Object.entries(originalValue)) {
       if (DataType.isObject(value)) {
         // Clean in depth before testing for walking.
-        value = await transformRecord(
-          value,
-          transformValue,
-          transformArray,
-          transformRecord
-        );
+        value = await this.walkRecord(value, transformValue);
       } else if (DataType.isArray(value)) {
-        value = await transformArray(
-          value as Array<any>,
-          transformValue,
-          transformArray,
-          transformRecord
-        );
+        value = await this.walkArray(value as Array<any>, transformValue);
       } else {
         value = await transformValue(value);
       }
@@ -198,29 +96,16 @@ export class DeepWalk {
 
   protected walkRecordSync(
     originalValue: Record<string, any>,
-    transformValue: TransformFunctionSync,
-    transformArray: TransformArrayFunctionSync,
-    transformRecord: TransformRecordFunctionSync
+    transformValue: TransformFunctionSync
   ): Record<string, any> {
     const newValue: Record<string, any> = {};
-
     // eslint-disable-next-line prefer-const
     for (let [key, value] of Object.entries(originalValue)) {
       if (DataType.isObject(value)) {
         // Clean in depth before testing for walking.
-        value = transformRecord(
-          value,
-          transformValue,
-          transformArray,
-          transformRecord
-        );
+        value = this.walkRecordSync(value, transformValue);
       } else if (DataType.isArray(value)) {
-        value = transformArray(
-          value as Array<any>,
-          transformValue,
-          transformArray,
-          transformRecord
-        );
+        value = this.walkArraySync(value as Array<any>, transformValue);
       } else {
         value = transformValue(value);
       }

b2dcd73821d85ecede9013ff570a92fb4134db58
Randy Merrill <randy@blinkk.com>
2021-05-04 17:26:31 -0600

    Add string type to project type for grow.

--- src/projectType/grow/growProjectType.ts
+++ src/projectType/grow/growProjectType.ts
@@ -2,6 +2,7 @@ import {FieldConstructor} from '@blinkk/selective-edit';
 import {GrowDocumentField} from './field/document';
 import {GrowPartialsField} from './field/partials';
 import {GrowStaticField} from './field/static';
+import {GrowStringField} from './field/string';
 import {ProjectTypeComponent} from '../projectType';
 
 export class GrowProjectType implements ProjectTypeComponent {
@@ -12,6 +13,7 @@ export class GrowProjectType implements ProjectTypeComponent {
       document: (GrowDocumentField as unknown) as FieldConstructor,
       partials: (GrowPartialsField as unknown) as FieldConstructor,
       static: (GrowStaticField as unknown) as FieldConstructor,
+      string: (GrowStringField as unknown) as FieldConstructor,
     };
   }
 }

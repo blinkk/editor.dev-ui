2f5caedb2e31f9b7a79c30856ea730ecc0ce4418
Randy Merrill <randy@blinkk.com>
2021-08-04 13:00:20 -0600

    feat: Glob style filter. (#193)
    Useful for doing glob style pattern matching.
    Gitignore style filters to be more friendly for git style glob matching.

--- src/ts/utility/filter.test.ts
+++ src/ts/utility/filter.test.ts
@@ -1,13 +1,13 @@
-import {IncludeExcludeFilter} from './filter';
+import {GitignoreFilter, GlobFilter, IncludeExcludeFilter} from './filter';
 import test from 'ava';
 
-test('filters without config', t => {
+test('filters include/exclude without config', t => {
   const filter = new IncludeExcludeFilter({});
 
   t.deepEqual(filter.filter(['a', 'b', 'c']), ['a', 'b', 'c']);
 });
 
-test('filters using includes string', t => {
+test('filters include/exclude using includes string', t => {
   const filter = new IncludeExcludeFilter({
     includes: ['[ab]'],
   });
@@ -15,7 +15,7 @@ test('filters using includes string', t => {
   t.deepEqual(filter.filter(['a', 'b', 'c']), ['a', 'b']);
 });
 
-test('filters using includes regex', t => {
+test('filters include/exclude using includes regex', t => {
   const filter = new IncludeExcludeFilter({
     includes: [/[ac]/],
   });
@@ -23,7 +23,7 @@ test('filters using includes regex', t => {
   t.deepEqual(filter.filter(['a', 'b', 'c']), ['a', 'c']);
 });
 
-test('filters using excludes string', t => {
+test('filters include/exclude using excludes string', t => {
   const filter = new IncludeExcludeFilter({
     excludes: ['[ab]'],
   });
@@ -31,7 +31,7 @@ test('filters using excludes string', t => {
   t.deepEqual(filter.filter(['a', 'b', 'c']), ['c']);
 });
 
-test('filters using excludes regex', t => {
+test('filters include/exclude using excludes regex', t => {
   const filter = new IncludeExcludeFilter({
     excludes: [/[ac]/],
   });
@@ -39,7 +39,7 @@ test('filters using excludes regex', t => {
   t.deepEqual(filter.filter(['a', 'b', 'c']), ['b']);
 });
 
-test('filters using includes and excludes string', t => {
+test('filters include/exclude using includes and excludes string', t => {
   const filter = new IncludeExcludeFilter({
     includes: ['[ab]'],
     excludes: ['[a]'],
@@ -48,11 +48,68 @@ test('filters using includes and excludes string', t => {
   t.deepEqual(filter.filter(['a', 'b', 'c']), ['b']);
 });
 
-test('filters using includes and excludes regex', t => {
+test('filters include/exclude using includes and excludes regex', t => {
   const filter = new IncludeExcludeFilter({
     includes: [/[ac]/],
     excludes: [/[c]/],
   });
 
   t.deepEqual(filter.filter(['a', 'b', 'c']), ['a']);
+});
+
+test('filters glob with simple glob patterns', t => {
+  const filter = new GlobFilter({
+    patterns: ['*.js'],
+  });
+
+  t.deepEqual(filter.filter(['a.md', 'b.js', 'c.txt']), ['b.js']);
+});
+
+test('filters glob with negated simple glob patterns', t => {
+  const filter = new GlobFilter({
+    negate: true,
+    patterns: ['*.js'],
+  });
+
+  t.deepEqual(filter.filter(['a.md', 'b.js', 'c.txt']), ['a.md', 'c.txt']);
+});
+
+test('filters glob with starting file patterns', t => {
+  const filter = new GlobFilter({
+    negate: true,
+    patterns: ['**/.*', '**/_*'],
+  });
+
+  t.deepEqual(
+    filter.filter([
+      'a.md',
+      'b.js',
+      'c.txt',
+      '/.config.json',
+      '/_config.json',
+      '/dir/file.txt',
+      '/dir/_config.txt',
+      '/dir/.config.txt',
+    ]),
+    ['a.md', 'b.js', 'c.txt', '/dir/file.txt']
+  );
+});
+
+test('filters gitignore with common ignore values', t => {
+  const filter = new GitignoreFilter({
+    patterns: ['node_modules', 'build', '*.pyc'],
+  });
+
+  t.deepEqual(
+    filter.filter([
+      '/content/a.md',
+      '/content/b.js',
+      '/content/c.txt',
+      '/node_modules/test/index.js',
+      '/build/index.html',
+      '/src/file.py',
+      '/src/file.pyc',
+    ]),
+    ['/content/a.md', '/content/b.js', '/content/c.txt', '/src/file.py']
+  );
 });
--- src/ts/utility/filter.ts
+++ src/ts/utility/filter.ts
@@ -1,4 +1,5 @@
 import {DataType} from '@blinkk/selective-edit/dist/utility/dataType';
+import minimatch from 'minimatch';
 
 export interface FilterComponent {
   filter(values: Array<string>): Array<string>;
@@ -6,10 +7,36 @@ export interface FilterComponent {
 }
 
 export interface IncludeExcludeFilterConfig {
+  /**
+   * If provided, the value needs to match at least one pattern
+   * to be considered as passing the filter.
+   */
   includes?: Array<string | RegExp>;
+  /**
+   * If provided, the value cannot match any of the patterns
+   * to be considered as passing the filter.
+   */
   excludes?: Array<string | RegExp>;
 }
 
+/**
+ * Filter that allows for defining a set of regular expressions
+ * for including a value or excluding a value based on the expressions.
+ *
+ * Depending on which config values are provided, the filtering
+ * works differently.
+ *
+ * If both the `includes` and `excludes` are defined in the config
+ * then the value being matched must match at least one of the
+ * patterns in the `includes` and cannot match any of the `exclude`
+ * patterns.
+ *
+ * If only `includes` are provided, the value must match at least
+ * one of the patterns.
+ *
+ * If only the `excludes` are provides, the value cannot match any
+ * of the patterns.
+ */
 export class IncludeExcludeFilter implements FilterComponent {
   config: IncludeExcludeFilterConfig;
 
@@ -66,6 +93,121 @@ export class IncludeExcludeFilter implements FilterComponent {
   }
 }
 
+export interface GlobFilterConfig {
+  /**
+   * When true, the results of the match are flipped.
+   *
+   * For instance, when you want to filter out ignored files
+   * you can use the `negate` option to filter out any matches.
+   */
+  negate?: boolean;
+  /**
+   * Value needs to match at least one of the patterns to
+   * pass the filtering.
+   */
+  patterns: Array<string>;
+}
+
+/**
+ * Filtering use glob style patterns.
+ */
+export class GlobFilter implements FilterComponent {
+  config: GlobFilterConfig;
+
+  constructor(config: GlobFilterConfig) {
+    this.config = config;
+  }
+
+  filter(values: Array<string>): Array<string> {
+    return values.filter(this.matches, this);
+  }
+
+  matches(value: string): boolean {
+    // If there are includes, it needs to match at least one of them.
+    let matchesPattern = false;
+    for (const pattern of this.config.patterns) {
+      if (minimatch(value, pattern)) {
+        matchesPattern = true;
+        break;
+      }
+    }
+
+    if (this.config.negate) {
+      return !matchesPattern;
+    }
+
+    return matchesPattern;
+  }
+}
+
+export interface GitignoreFilterConfig {
+  /**
+   * Value needs to match at least one of the patterns to
+   * pass the filtering.
+   */
+  patterns: Array<string>;
+}
+
+/**
+ * Filtering use gitignore glob style patterns.
+ */
+export class GitignoreFilter implements FilterComponent {
+  config: GitignoreFilterConfig;
+
+  constructor(config: GitignoreFilterConfig) {
+    this.config = config;
+
+    // Convert the patterns to gitignore style globs.
+    this.config.patterns = this.convertPatterns(this.config.patterns);
+  }
+
+  /**
+   *
+   * @param values Raw patterns for ignored filters
+   * @returns
+   */
+  protected convertPatterns(values: Array<string>): Array<string> {
+    const patterns: Array<string> = [];
+    for (const pattern of values) {
+      const prefix = pattern.charAt(0) !== '/' ? '**/' : '';
+      if (!isFileGlobPattern(pattern)) {
+        const suffix = pattern.slice(-1) === '*' ? '*' : '/**';
+        if (pattern.slice(-1) === '/') {
+          patterns.push(`${prefix}${pattern}${suffix}`);
+        } else {
+          // Create pair of globs.
+          patterns.push(`${prefix}${pattern}`);
+          patterns.push(`${prefix}${pattern}${suffix}`);
+        }
+      } else {
+        patterns.push(pattern);
+        if (pattern.startsWith('*') && !pattern.startsWith('**')) {
+          patterns.push(`${prefix}${pattern}`);
+        }
+      }
+    }
+    return patterns;
+  }
+
+  filter(values: Array<string>): Array<string> {
+    return values.filter(this.matches, this);
+  }
+
+  matches(value: string): boolean {
+    // If there are includes, it needs to match at least one of them.
+    let matchesPattern = false;
+    for (const pattern of this.config.patterns) {
+      if (minimatch(value, pattern)) {
+        matchesPattern = true;
+        break;
+      }
+    }
+
+    // Using ignores, which means we want to remove matches.
+    return !matchesPattern;
+  }
+}
+
 /**
  * Escapes a string to be used as a 'constant' in a regex.
  *
@@ -74,4 +216,8 @@ export class IncludeExcludeFilter implements FilterComponent {
  */
 export function escapeRegExp(value: string) {
   return value.replace(/[.*+?^${}()|[\]\\/]/g, '\\$&');
+}
+
+function isFileGlobPattern(pattern: string) {
+  return pattern == '*' || pattern.indexOf('.') !== -1;
 }
